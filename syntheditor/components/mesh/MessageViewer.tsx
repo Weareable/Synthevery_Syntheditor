43dee0994e4b7e3cae9335097eb9c8946c15a7e9
wachinaba <wachinaba@gmail.com>
2024-12-31 14:32:03 +0900

    add Type指定機能

--- syntheditor/components/mesh/MessageViewer.tsx
+++ syntheditor/components/mesh/MessageViewer.tsx
@@ -4,12 +4,15 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useMeshContext } from '@/providers/mesh-provider';
 import { MeshPacket } from '@/types/mesh';
 
-const MessageViewer = () => {
+interface MessageViewerProps {
+    messageType: number;
+}
+
+const MessageViewer = ({ messageType }: MessageViewerProps) => {
     const { setCallback, removeCallback } = useMeshContext();
     const [messages, setMessages] = useState<MeshPacket[]>([]);
 
     useEffect(() => {
-        const messageType = 255; // 受信したいメッセージタイプ
         const handleMessage = (packet: MeshPacket) => {
             console.log('Received packet:', packet);
             setMessages((prevMessages) => [...prevMessages, packet]);
@@ -20,7 +23,7 @@ const MessageViewer = () => {
         return () => {
             removeCallback(messageType); // クリーンアップ時にコールバック解除
         };
-    }, [setCallback, removeCallback]); // 最小限の依存関係
+    }, [setCallback, removeCallback, messageType]); // 最小限の依存関係
 
     return (
         <div>
@@ -29,8 +32,8 @@ const MessageViewer = () => {
                 {messages.map((msg, index) => (
                     <li key={index}>
                         <strong>Type:</strong> {msg.type}, <strong>Source:</strong>{' '}
-                        {Array.from(msg.source.address).map((byte) => byte.toString(16).padStart(2, '0')).join(':')},{' '}
-                        <strong>Data:</strong> {Array.from(msg.data).join(', ')}
+                        {Array.from(msg.source.address.reverse()).map((byte) => byte.toString(16).padStart(2, '0')).join(':')},{' '}
+                        <strong>Data:</strong> {Array.from(msg.data).map((byte) => byte.toString(16).padStart(2, '0')).join(' ')}
                     </li>
                 ))}
             </ul>
