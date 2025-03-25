0b458464f4ecbdc8b31f0d500c8e73a8353df827
wachinaba <wachinaba@gmail.com>
2025-03-25 21:16:27 +0900

    add useAppStateフック

---
+++ syntheditor/hooks/useAppState.ts
@@ -0,0 +1,27 @@
+import { useReducer, useEffect } from "react";
+import { mesh } from "@/lib/synthevery-core/connection/mesh";
+import { getAddressString } from "@/lib/synthevery-core/connection/util";
+import { P2PMacAddress } from "@/lib/synthevery-core/types/mesh";
+import { playerSyncStates } from "@/lib/synthevery-core/player/states";
+import { SyncState } from "@/lib/synthevery-core/appstate/appstates";
+
+export default function useAppState<T>(syncState: SyncState<T>): [T, (newValue: T) => void] {
+    const [state, setState] = useReducer((currentValue: T, newValue: T) => {
+        syncState.getStore().value = newValue;
+        syncState.notifyChange();
+        return newValue;
+    }, syncState.getStore().value);
+
+    useEffect(() => {
+        const callback = () => {
+            setState(syncState.getStore().value);
+        };
+
+        syncState.eventEmitter.on('synced', callback);
+        return () => {
+            syncState.eventEmitter.off('synced', callback);
+        };
+    }, [syncState]);
+
+    return [state, setState];
+}
