c8aa78ebed4e40ddcb635f5180f66be03a3d87e3
wachinaba <wachinaba@gmail.com>
2025-03-26 01:03:01 +0900

    add ReadOnly用フック

--- syntheditor/hooks/useAppState.ts
+++ syntheditor/hooks/useAppState.ts
@@ -1,11 +1,11 @@
-import { useReducer, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import { mesh } from "@/lib/synthevery-core/connection/mesh";
 import { getAddressString } from "@/lib/synthevery-core/connection/util";
 import { P2PMacAddress } from "@/lib/synthevery-core/types/mesh";
 import { playerSyncStates } from "@/lib/synthevery-core/player/states";
-import { SyncState } from "@/lib/synthevery-core/appstate/appstates";
+import { SyncState, ReadOnlySyncState } from "@/lib/synthevery-core/appstate/appstates";
 
-export default function useAppState<T>(syncState: SyncState<T>): [T, (newValue: T) => void] {
+export function useAppState<T>(syncState: SyncState<T>): [T, (newValue: T) => void] {
     const [state, setState] = useReducer((currentValue: T, newValue: T) => {
         syncState.getStore().value = newValue;
         syncState.notifyChange();
@@ -24,4 +24,21 @@ export default function useAppState<T>(syncState: SyncState<T>): [T, (newValue: 
     }, [syncState]);
 
     return [state, setState];
+}
+
+export function useReadOnlyAppState<T>(syncState: ReadOnlySyncState<T>): T {
+    const [state, setState] = useState(syncState.getStore().value);
+
+    useEffect(() => {
+        const callback = () => {
+            setState(syncState.getStore().value);
+        };
+
+        syncState.eventEmitter.on('synced', callback);
+        return () => {
+            syncState.eventEmitter.off('synced', callback);
+        };
+    }, [syncState]);
+
+    return state;
 }
