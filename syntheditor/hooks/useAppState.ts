45e2cd2d4348633fd6a6dd965a22fa3d58a36287
wachinaba <wachinaba@gmail.com>
2025-03-27 23:04:22 +0900

    fix オブジェクト変更判定

--- syntheditor/hooks/useAppState.ts
+++ syntheditor/hooks/useAppState.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { mesh } from "@/lib/synthevery-core/connection/mesh";
 import { getAddressString } from "@/lib/synthevery-core/connection/util";
 import { P2PMacAddress } from "@/lib/synthevery-core/types/mesh";
@@ -6,11 +6,21 @@ import { playerSyncStates } from "@/lib/synthevery-core/player/states";
 import { SyncState, ReadOnlySyncState } from "@/lib/synthevery-core/appstate/appstates";
 
 export function useAppState<T>(syncState: SyncState<T>): [T, (newValue: T) => void] {
+    /*
     const [state, setState] = useReducer((currentValue: T, newValue: T) => {
         syncState.getStore().value = newValue;
         syncState.notifyChange();
         return newValue;
-    }, syncState.getStore().value);
+    }, syncState.getStore().value); 
+    */
+
+    const [state, setState_] = useState(syncState.getStore().value);
+
+    const setState = useCallback((newValue: T) => {
+        syncState.getStore().value = newValue;
+        syncState.notifyChange();
+        setState_(() => { return newValue; });
+    }, [syncState, setState_]);
 
     useEffect(() => {
         const callback = () => {
@@ -21,7 +31,7 @@ export function useAppState<T>(syncState: SyncState<T>): [T, (newValue: T) => vo
         return () => {
             syncState.eventEmitter.off('synced', callback);
         };
-    }, [syncState]);
+    }, [syncState, setState]);
 
     return [state, setState];
 }
@@ -31,7 +41,9 @@ export function useReadOnlyAppState<T>(syncState: ReadOnlySyncState<T>): T {
 
     useEffect(() => {
         const callback = () => {
-            setState(syncState.getStore().value);
+            setState(() => {
+                return syncState.getStore().value;
+            });
         };
 
         syncState.eventEmitter.on('synced', callback);
