052ae8616d64471e5f3df1f54c9dbabb72102308
wachinaba <wachinaba@gmail.com>
2025-05-17 20:42:59 +0900

    delete 不要インポート

--- syntheditor/app/layout.tsx
+++ syntheditor/app/layout.tsx
@@ -1,9 +1,6 @@
 // app/layout.tsx
 import './globals.css';
 import { Metadata } from 'next';
-import { BLEProvider } from '@/providers/ble-provider';
-import { MeshProvider } from '@/providers/mesh-provider';
-
 export const metadata: Metadata = {
   title: 'My BLE + Mesh App',
   description: 'A Next.js app with BLE and Mesh functionalities',
