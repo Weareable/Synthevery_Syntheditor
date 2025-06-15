5f10a02679de916da99d03756b6880d3212679ff
wachinaba <wachinaba@gmail.com>
2025-06-15 20:57:43 +0900

    fix panel.tsxのスタイルを修正し、flexの方向を縦から横に変更

--- syntheditor/components/ui/panel.tsx
+++ syntheditor/components/ui/panel.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const panelVariants = cva(
-    "flex flex-col rounded-lg bg-card text-card-foreground",
+    "flex rounded-lg bg-card text-card-foreground",
     {
         variants: {
             variant: {
