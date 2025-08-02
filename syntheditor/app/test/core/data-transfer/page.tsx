421f77bd201fc6dd6f474288815e0539091121ec
wachinaba <wachinaba@gmail.com>
2025-08-02 21:07:10 +0900

    feat NoteBuilderConfigの受信テスト機能を追加

--- syntheditor/app/test/core/data-transfer/page.tsx
+++ syntheditor/app/test/core/data-transfer/page.tsx
@@ -5,7 +5,8 @@ import { mesh } from '@/lib/synthevery-core/connection/mesh';
 import { getAddressFromString, getAddressString } from '@/lib/synthevery-core/connection/util';
 
 import { dataTransferController, MockSenderDataStore } from '@/lib/synthevery-core/data-transfer/data-transfer-controller';
-import { sendGeneratorConfig, sendNoteBuilderConfig } from '@/lib/synthevery-core/player/config';
+import { sendGeneratorConfig, sendNoteBuilderConfig, NoteBuilderConfigReceiverPort, GeneratorConfigReceiverPort } from '@/lib/synthevery-core/player/config';
+import { playerController } from '@/lib/synthevery-core/player/controller';
 
 dataTransferController;
 
@@ -22,6 +23,23 @@ const DataTransferExample: React.FC = () => {
     }
 
     useEffect(() => {
+        // Receiver portsを登録
+        const noteBuilderConfigReceiverPort = new NoteBuilderConfigReceiverPort();
+        const generatorConfigReceiverPort = new GeneratorConfigReceiverPort();
+
+        // 受信イベントのリスナーを設定
+        noteBuilderConfigReceiverPort.eventEmitter.on('received', (config) => {
+            console.log('Received NoteBuilderConfig:', config);
+        });
+
+        generatorConfigReceiverPort.eventEmitter.on('received', (config) => {
+            console.log('Received GeneratorConfig:', config);
+        });
+
+        // DataTransferControllerにreceiver portsを登録
+        dataTransferController.registerReceiverPort(noteBuilderConfigReceiverPort);
+        dataTransferController.registerReceiverPort(generatorConfigReceiverPort);
+
         mesh.eventEmitter.on('peerConnected', updatePeerDevices);
         mesh.eventEmitter.on('peerDisconnected', updatePeerDevices);
 
@@ -39,6 +57,12 @@ const DataTransferExample: React.FC = () => {
         sendGeneratorConfig(peerAddress, [{ class: "sf", params: { filename: "/rock_drum.sf2", preset_index: 9, is_drum: true } }, { class: "sf", params: { filename: "/rock_drum.sf2", preset_index: 9, is_drum: true } }, { class: "sf", params: { filename: "/rock_drum.sf2", preset_index: 9, is_drum: true } }]);
     }, []);
 
+    const retrieveConfig = useCallback((peer: string) => {
+        const peerAddress = getAddressFromString(peer);
+        const config = playerController.requestNoteBuilderConfig(peerAddress);
+        console.log(config);
+    }, []);
+
     return (
         <div>
             <div>
@@ -49,6 +73,7 @@ const DataTransferExample: React.FC = () => {
                 {peerDevices.map(device => <div key={device}>
                     {device}
                     <button onClick={() => sendConfig(device)}>Send</button>
+                    <button onClick={() => retrieveConfig(device)}>Retrieve</button>
                 </div>)}
             </div>
         </div>
