144f5b623efbe57d92b73853668d69559871d5d2
wachinaba <wachinaba@gmail.com>
2025-08-23 15:45:39 +0900

    refactor useDeviceControlフックから不要な状態管理を削除し、シンプル化

--- syntheditor/hooks/useDeviceControl.ts
+++ syntheditor/hooks/useDeviceControl.ts
@@ -1,13 +1,8 @@
-import { useState, useEffect, useCallback } from "react";
+import { useCallback } from "react";
 import { useSynthevery } from "@/contexts/SyntheveryContext";
-import { useReadOnlyAppState } from "./useAppState";
 
 export default function useDeviceControl() {
-    const { deviceController, playerSyncStates } = useSynthevery();
-    const tickClockState = useReadOnlyAppState(playerSyncStates.tickClockState);
-
-    const playingState = tickClockState.playing;
-    const bpmState = tickClockState.bpm;
+    const { deviceController } = useSynthevery();
 
     const setPlayingState = useCallback((playing: boolean) => {
         deviceController.setPlayingState(playing ? "play" : "pause");
@@ -21,5 +16,5 @@ export default function useDeviceControl() {
         deviceController.setPlayingState("stop");
     }, []); // オブジェクトが不変なので空でOK！
 
-    return { playingState, bpmState, setPlayingState, setBpmState, stop };
+    return { setPlayingState, setBpmState, stop };
 }
